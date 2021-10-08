addc07be3a097ae00d61635e1eaf1fb3a4b272ff
alexjohnson9551 <91478113+alexjohnson9551@users.noreply.github.com>
2021-10-08 13:25:57 -0500

    Update index.tsx

--- Frontend/proj2front/src/index.tsx
+++ Frontend/proj2front/src/index.tsx
@@ -8,7 +8,7 @@ import Controller from './components/Controller/Controller';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Controller />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
