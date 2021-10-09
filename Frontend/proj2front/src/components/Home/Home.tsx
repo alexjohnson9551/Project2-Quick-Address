3d4bcad1a3d5fa166963433129871954c23f3db0
alexjohnson9551 <alex.johnson@revature.net>
2021-10-09 13:56:07 -0500

    structure navbar and page flow and increase fade in speed

--- Frontend/proj2front/src/components/Home/Home.tsx
+++ Frontend/proj2front/src/components/Home/Home.tsx
@@ -13,6 +13,11 @@ const Home = (props: any) => {
 
   return (<div>
     <h1>Welcome {props.username}</h1>
+    <p>
+      Three sensible options for "home" page: 1) this is the same as the page to view addresses they've entered <br/>
+       2) this is the same as the page to enter a new address <br/> 
+       3) this is another page altogether, perhaps showing personal info, and probably big buttons to go to either of the above mentioned pages
+      </p>
     <br/>
     <Button onClick={() => logout()}>Logout</Button>
   </div>);
