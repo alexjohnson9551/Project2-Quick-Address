2b3ae85009280b907bc6e8c94c97560f4524223f
Will Chnowski <william.chnowski@gmail.com>
2021-10-13 11:35:11 -0400

    routing

--- Frontend/proj2front/src/App.tsx
+++ Frontend/proj2front/src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import {BrowserRouter as CoolRouter, Switch, Route} from 'react-router-dom';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Controller from './components/Controller/Controller';
 import Registration from './components/Registration/Registration';
 import MapContainer from './components/Map/ShowMap';
+
 
 interface AppProps{
 }
@@ -12,24 +13,26 @@ interface AppProps{
 const App : React.FC<AppProps> = () => {
   return (
     <div className="App">
+      <Router>
         <Controller />
+      </Router>
     </div>
   );
 }
 
 //Example of a simple router...currently not being used
-function Router() {
-  return (
-    <CoolRouter>
-      <div className="CoolRouter">
-        <Switch>
-          <Route path ="/" component={Registration}></Route>
-          <Route path ="/map" component={MapContainer}></Route>
-        </Switch>
-      </div>
-    </CoolRouter>
-  );
-}
+// function Router() {
+//   return (
+//     <CoolRouter>
+//       <div className="CoolRouter">
+//         <Switch>
+//           <Route path ="/" component={Registration}></Route>
+//           <Route path ="/map" component={MapContainer}></Route>
+//         </Switch>
+//       </div>
+//     </CoolRouter>
+//   );
+// }
 
 export type {AppProps};
 export default App;
