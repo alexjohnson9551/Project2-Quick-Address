e5c7849b853cbb069c95e073706fa22da41c4149
RazzzyK <Razenok@yahoo.com>
2021-10-14 01:58:30 -0400

    Map is on one side of the Home Page now

--- Frontend/proj2front/src/components/Home/Home.tsx
+++ Frontend/proj2front/src/components/Home/Home.tsx
@@ -2,7 +2,9 @@ import React from 'react';
 import { Button } from 'react-bootstrap';
 import NavbarToggle from 'react-bootstrap/esm/NavbarToggle';
 import { useHistory } from 'react-router';
+import MyGoogleMap from '../Map/MyGoogleMaps';
 import Navigation from '../Navigation/Navigation';
+import './homeStyle.css';
 
 
 const Home = (props: any) => {
@@ -20,9 +22,23 @@ const Home = (props: any) => {
       The left side will contain the map to add addresses, the right side will contain a list of the user's addresses.
       </p>
     <br/>
-    <Button onClick={() => logout()}>Logout</Button>
+    <div className="container">
+      <div className="row">
+        <div className="col column1 main-wrapper">
+          <MyGoogleMap/>
+        </div>
+        <div className="col">
+          <p>TEST</p>
+          PUT THE TABLE HERE WOOOOO
+          
+        </div>
+        
+      </div>
+    </div>
+    <div><Button onClick={() => logout()}>Logout</Button></div>
   </div>);
-
+  
+  
 };
 
 export default Home;
