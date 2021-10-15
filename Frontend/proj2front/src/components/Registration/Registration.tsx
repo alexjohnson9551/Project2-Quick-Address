961d9956d485068119c22a7b8950764058431de1
alexjohnson9551 <alex.johnson@revature.net>
2021-10-14 19:02:16 -0500

    full registration

--- Frontend/proj2front/src/components/Registration/Registration.tsx
+++ Frontend/proj2front/src/components/Registration/Registration.tsx
@@ -18,33 +18,32 @@ const RegPage = (props: any) => {
   const nextPageHandler = props.nextPageHandler;
 
   //Deprecated as of now...
-  let firstnameChangeHandler = (e:any) => {
+  let firstnameChangeHandler = (e: any) => {
     setUserFirstname(e.target.value);
     console.log(userFirstname);
   }
 
-  let lastnameChangeHandler = (e:any) => {
+  let lastnameChangeHandler = (e: any) => {
     setUserLastname(e.target.value);
     console.log(userLastname);
   }
 
-  let passChangeHandler = (e:any) => {
+  let passChangeHandler = (e: any) => {
     setUserPass(e.target.value);
     console.log(userPass);
   }
 
-  let userNameChangeHandler = (e:any) => {
+  let userNameChangeHandler = (e: any) => {
     setUserName(e.target.value);
     console.log(userName);
   }
-  
-  let emailChangeHandler = (e:any) => {
+
+  let emailChangeHandler = (e: any) => {
     setUserEmail(e.target.value);
     console.log(userEmail);
   }
 
-  function sendToDB()
-  {
+  function sendToDB() {
     let toSend = {
       firstName: userFirstname,
       lastName: userLastname,
@@ -53,49 +52,50 @@ const RegPage = (props: any) => {
       email: userEmail
     };
 
-    if(userFirstname === '' || userLastname === '' || userName === '' || userPass === '' || userEmail === ''){
+    if (userFirstname === '' || userLastname === '' || userName === '' || userPass === '' || userEmail === '') {
       alert("Please fill out all parts of the form!");
       return;
     }
 
-    if(!userEmail.match(/^([\w.%+-]+)@([\w-]+\.)+([\w]{2,})$/i)) {
+    if (!userEmail.match(/^([\w.%+-]+)@([\w-]+\.)+([\w]{2,})$/i)) {
       alert("Invalid Email format!");
       return;
     }
 
-    if(userPass.length < 4 || userName.length < 4) {
+    if (userPass.length < 4 || userName.length < 4) {
       alert("Username and/ord Password is too short, needs to be more than 4 characters!")
       return;
     }
 
     let json = JSON.stringify(toSend);
-    
+
 
     console.log("POST WITH AXIOS! Sending: " + json);
 
     axios.post(
       'http://localhost:8080/test/url/users',
       json,
-      
-      {headers: {'Content-Type': "application/json"}}).then(  //Do I need withCredentials since this is a registration page
-          res => {
-            
-            if(res.data == 'Username is Registered' || res.data == 'Email is Registered')
-            {
-              alert(res.data);
-              //Need to clear the form here
-            }
-            else
-              props.nextPageHandler("Login")
-        })
-        .catch((err) => {
-          console.log({err});
-          alert("Error: " + err.response);
-        });
 
-        //Something to do with routing...
-        //props.history.push('/map');
-        
+      { headers: { 'Content-Type': "application/json" } }).then(  //Do I need withCredentials since this is a registration page
+        res => {
+
+          if (res.data == 'Username is Registered' || res.data == 'Email is Registered') {
+            alert(res.data);
+            //Need to clear the form here
+          }
+          else
+            alert("User Registration Successful!");
+            props.nextPageHandler("Login")
+            history.push("Login");
+        })
+      .catch((err) => {
+        console.log({ err });
+        alert("Error: " + err.response);
+      });
+
+    //Something to do with routing...
+    //props.history.push('/map');
+
   }
 
   return (
@@ -111,35 +111,35 @@ const RegPage = (props: any) => {
 
                 <div className="inputStyle">
                   <label>First Name</label>
-                  <input type="text" id="" className="test" value={userFirstname} onChange={(e:any) => {setUserFirstname(e.target.value)}}/>
-                  
+                  <input type="text" id="" className="test" value={userFirstname} onChange={(e: any) => { setUserFirstname(e.target.value) }} />
+
                 </div>
 
                 <div className="inputStyle">
                   <label>Last Name</label>
-                  <input type="text" id="" className="test" value={userLastname} onChange={(e:any) => {setUserLastname(e.target.value)}}/>
-                  
+                  <input type="text" id="" className="test" value={userLastname} onChange={(e: any) => { setUserLastname(e.target.value) }} />
+
                 </div>
 
                 <div className="inputStyle">
                   <label className="">Username</label>
-                  <input type="text" id="" className="test" value={userName} onChange={(e:any) => {setUserName(e.target.value)}}/>
-                  
+                  <input type="text" id="" className="test" value={userName} onChange={(e: any) => { setUserName(e.target.value) }} />
+
                 </div>
 
                 <div className="inputStyle">
                   <label className="">Password</label>
-                  <input type="password" id="" className="test" value={userPass} onChange={(e:any) => {setUserPass(e.target.value)}}/>
-                  
+                  <input type="password" id="" className="test" value={userPass} onChange={(e: any) => { setUserPass(e.target.value) }} />
+
                 </div>
 
                 <div className="inputStyle">
                   <label className="">Email</label>
-                  <input type="email" id="" className="test" value={userEmail} onChange={(e:any) => {setUserEmail(e.target.value)}}/>
+                  <input type="email" id="" className="test" value={userEmail} onChange={(e: any) => { setUserEmail(e.target.value) }} />
                 </div>
 
                 <div className="d-flex justify-content-center buttons">
-                  <Button onClick={() => {nextPageHandler("Login"); history.push("/")}}>Back to Login</Button>
+                  <Button onClick={() => { nextPageHandler("Login"); history.push("/") }}>Back to Login</Button>
                   <Button className="btnStyle" onClick={() => sendToDB()}>Create Account</Button>
                 </div>
 
--- proj2backend/proj2backend/src/main/java/com/revature/proj2backend/rest/controller/UserController.java
+++ proj2backend/proj2backend/src/main/java/com/revature/proj2backend/rest/controller/UserController.java
@@ -1,4 +1,28 @@
 package com.revature.proj2backend.rest.controller;
-public class UserController {
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.revature.proj2backend.model.entities.Users;
+import com.revature.proj2backend.registration.UserService;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+	
+	@Autowired
+	private UserService userService;
+
+	@PostMapping
+	public ResponseEntity<String> create(@RequestBody Users user) {
+		if(user.getUserID() != 0) {
+			return ResponseEntity.badRequest().build();
+		}
+		return ResponseEntity.ok(userService.addNewUser(user));
+	}
+	
 }
